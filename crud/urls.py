a2be6c24b98ec8866e1db8b20e1dd3c25f100353
Manuel Cristóbal Roldán Gómez <xdmrgxd@uma.es>
2021-11-27 18:31:54 +0100

    Barras añadidas a urls

--- crud/urls.py
+++ crud/urls.py
@@ -3,9 +3,9 @@ from crud import views
 
 urlpatterns = [
     path('usuario/', views.UsuarioList.as_view()),
-    path('usuario/<int:pk>', views.UsuarioDetail.as_view()),
-    path('usuario/<str:name>', views.UsuarioPorNombre.as_view()),
-    path('usuario/<int:author>/comentario', views.UsuarioComentarios.as_view()),
+    path('usuario/<int:pk>/', views.UsuarioDetail.as_view()),
+    path('usuario/<str:name>/', views.UsuarioPorNombre.as_view()),
+    path('usuario/<int:author>/comentario/', views.UsuarioComentarios.as_view()),
     path('comentario/', views.ComentarioList.as_view()),
     path('comentario/<int:pk>/', views.ComentarioDetail.as_view()),
     path('comentario/<slug:date>/', views.ComentariosFecha.as_view())
