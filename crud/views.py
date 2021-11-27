33769e1643754333c30b9d3172ae42015040fc07
Manuel Cristóbal Roldán Gómez <xdmrgxd@uma.es>
2021-11-27 18:28:11 +0100

    Ejercicio 1 con Djongo finalizado

--- crud/views.py
+++ crud/views.py
@@ -5,6 +5,8 @@ from rest_framework.utils import serializer_helpers
 from rest_framework.views import APIView
 from .models import Usuario, Comentario
 from .serializers import UsuarioSerializer, ComentarioSerializer
+from django.db.models import Q
+import datetime
 
 # Create your views here.
 #Read-Write: Lista de usuarios
@@ -30,5 +32,20 @@ class ComentarioDetail(generics.RetrieveUpdateDestroyAPIView):
 class UsuarioComentarios(APIView):
     def get(self, request, author):
         comentarios = Comentario.objects.filter(author=author).order_by('-date')
+        serializer = ComentarioSerializer(comentarios, many=True)
+        return Response(serializer.data)
+
+class UsuarioPorNombre(APIView):
+    def get(self, request, name):
+        usuario = Usuario.objects.filter(Q(name__icontains=name) | Q(surname__icontains=name))
+        serializer = UsuarioSerializer(usuario, many=True)
+        return Response(serializer.data)
+
+class ComentariosFecha(APIView):
+    """Format 2021-12-31"""
+    def get(self, request, date):
+        dateArray = date.split('-')
+        datetime.date(int(dateArray[0]), int(dateArray[1]), int(dateArray[2]))
+        comentarios = Comentario.objects.filter(date__lte=date).order_by('-date')
         serializer = ComentarioSerializer(comentarios, many=True)
         return Response(serializer.data)
