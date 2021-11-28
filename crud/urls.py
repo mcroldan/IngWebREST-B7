20068fb1481fda7b2e4f39da6d90c675fe0f0b9e
Antonio <antcandel@uma.es>
2021-11-28 19:28:06 +0100

    swagger openapi
    implementacion swagger con drf-yasq django

--- crud/urls.py
+++ crud/urls.py
@@ -1,5 +1,22 @@
 from django.urls import path
 from crud import views
+from django.conf.urls import url
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="CRUD API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('usuario/', views.UsuarioList.as_view()),
@@ -8,6 +25,8 @@ urlpatterns = [
     path('usuario/<int:author>/comentario/', views.UsuarioComentarios.as_view()),
     path('comentario/', views.ComentarioList.as_view()),
     path('comentario/<int:pk>/', views.ComentarioDetail.as_view()),
-    path('comentario/<slug:date>/', views.ComentariosFecha.as_view())
-    
+    path('comentario/<slug:date>/', views.ComentariosFecha.as_view()),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- servidorREST/settings.py
+++ servidorREST/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'crud',
     'datos_abiertos',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
